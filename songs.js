852113941d56038ed755e57445dba19fac5a8f07
Vignesh lagishetti <88646053+buntyanvi@users.noreply.github.com>
2023-07-01 13:36:02 +0530

    Add files via upload

---
+++ songs.js
@@ -0,0 +1,46 @@
+"use strict";
+
+let songs = [
+  {
+    name: "Starboy",
+    img: "https://th.bing.com/th/id/R.f281562c2c4bd152aea50ec18668c835?rik=ch75TF4zlMYPVg&riu=http%3a%2f%2fthatgrapejuice.net%2fwp-content%2fuploads%2f2016%2f09%2fthe-weekend-starboy-thatgrapejuice.jpg&ehk=UY8ZVSk%2frLCwzkat%2b4DfL476%2fohx0TwePUDW8%2f33FxU%3d&risl=&pid=ImgRaw&r=0",
+    artist: "The Weeknd",
+    audio: "https://res.cloudinary.com/dd17x6cxq/video/upload/v1634064206/player%20music/khachaturian_waltz_.mp3"
+  },
+  {
+    name: "Blinding Lights",
+    img: "https://media.senscritique.com/media/000019036695/source_big/Blinding_Lights_Single.jpg",
+    artist: "The Weekend",
+    audio: "https://res.cloudinary.com/dd17x6cxq/video/upload/v1634064131/player%20music/bizet_suite.mp3"
+  },
+  {
+    name: "See You Again",
+    img: "https://th.bing.com/th/id/R.483138f7c6d2d5cd2bbdf09e2446cb18?rik=5lKdHjgYa9wJNA&riu=http%3a%2f%2fakns-images.eonline.com%2feol_images%2fEntire_Site%2f2017611%2frs_1024x422-170711084028-Wiz-Khalifa.jpg&ehk=vZTVQteXtM2hF8MQ6j0%2bMafhBbqq8jzH2wnRnZF4P5E%3d&risl=&pid=ImgRaw&r=0",
+    artist: "Wiz Khalifa",
+    audio: "https://res.cloudinary.com/dd17x6cxq/video/upload/v1634066178/player%20music/mozart_symphony.mp3"
+  },
+  {
+   name: "Attention",
+    img: "https://upload.wikimedia.org/wikipedia/commons/a/a3/Charlie_Puth_-_Attention_(Official_Single_Cover).png",
+    artist: "Charlie Puth",
+    audio: "https://res.cloudinary.com/dd17x6cxq/video/upload/v1634064131/player%20music/bizet_suite.mp3"
+  },
+  {
+    name: "Dandelions",
+    img: "https://th.bing.com/th/id/OIP.sXLE6_oX2Uv1v47kXNqq8AHaHa?w=175&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7",
+    artist: "Ruth B",
+    audio: "https://res.cloudinary.com/dd17x6cxq/video/upload/v1634064202/player%20music/tchaikovsky_waltz.mp3"
+  },
+  {
+    name: "Super Man",
+    img: "https://th.bing.com/th/id/R.d4384f63021fd88f6cc90a393929dc56?rik=6vHKM%2bZSr74ISw&riu=http%3a%2f%2fi1.ytimg.com%2fvi%2fRyALAhOOFxM%2fmaxresdefault.jpg&ehk=TjXIHb5iBEmOFDgzXG2xAbYFSyuFADGHU2bpeK3lhrk%3d&risl=&pid=ImgRaw&r=0",
+    artist: "Eminem",
+    audio: "https://res.cloudinary.com/dd17x6cxq/video/upload/v1634064206/player%20music/khachaturian_waltz_.mp3"
+  },
+  {
+    name: "Perfect",
+    img: "https://i.ytimg.com/vi/KrkAjnc_DEk/maxresdefault.jpg",
+    artist: "ED Sheeran",
+    audio: "https://res.cloudinary.com/dd17x6cxq/video/upload/v1634064196/player%20music/rachmaninof_prelude_N5.mp3"
+  }
+];
